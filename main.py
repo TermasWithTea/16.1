324fa4f8b7f0c14d9fa0331b22ef5fb27a962363
TermasWithTea <167928100+TermasWithTea@users.noreply.github.com>
2024-12-08 17:39:54 +0300

    main.py

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/')
-async def welcome() -> dict:
+async def welcome() -> str:
     return{'message':'Главная страница'}
 
 @app.get('/user/admin')
-async def ad_user() :
+async def ad_user() -> str:
     return{'message':'Вы вошли как администратор'}
 
 @app.get('/user/{user_id}')
-async def user_id(user_id:int) -> dict:
+async def user_id(user_id:int) -> str:
     return{'message':f'Вы вошли как пользователь под номером {user_id}'}
 
 @app.get('/id')
-async def end(username: str = 'alex', age: int = 24) -> dict:
+async def end(username: str = 'alex', age: int = 24) -> str:
     return{'message':'Информация о пользователе.','Name':username, 'Age': age}
 
